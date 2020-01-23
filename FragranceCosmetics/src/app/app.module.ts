d8592dd1f2bb5768eafe9a1f2b6a8a295bff985b
Jeeva <jeevageee25@gmail.com>
2020-01-23 11:37:28 +0530

    Added slideshow

--- FragranceCosmetics/src/app/app.module.ts
+++ FragranceCosmetics/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 
+import {SlideshowModule} from 'ng-simple-slideshow';  
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { LandingComponent } from './landing/landing.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlideshowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FragranceCosmetics/src/app/landing/landing.component.ts
+++ FragranceCosmetics/src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IImage } from 'ng-simple-slideshow/src/app/modules/slideshow/IImage';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  imageUrls: any = []
+  height: string = '400px';
+  arrowSize: string = '30px';
+  
   constructor() { }
 
   ngOnInit() {
+    this.prepareImageArray();
+  }
+
+  prepareImageArray(){
+    for(let i=1;i<=9;i++){
+      let obj =  {}
+      obj['url'] =`../../assets/images/`+i+`.jpg`;
+      this.imageUrls.push(obj)}
   }
 
 }
